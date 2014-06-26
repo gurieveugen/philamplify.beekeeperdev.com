bfab781587e21c06b27e59066f3ca47bbba1fd66
gurieveugen <gurievinvest@gmail.com>
2014-06-26 09:42:39 +0300

    0.22

--- page-salesforce.php
+++ page-salesforce.php
@@ -26,13 +26,12 @@ $email = isset($_GET['email']) ? $_GET['email'] : '';
 			<!--  ----------------------------------------------------------------------  -->
 			<!--  NOTE: These fields are optional debugging elements. Please uncomment    -->
 			<!--  these lines if you wish to test in debug mode.                          -->
-			<!-- <input type="hidden" name="debug" value=1>                      
-			<input type="hidden" name="debugEmail" value="tatarinfamily@gmail.com">   -->                    
+			<!-- <input type="hidden" name="debug" value=1> <input type="hidden" name="debugEmail" value="tatarinfamily@gmail.com">   -->                    
 			<!--  ----------------------------------------------------------------------  -->
 
-			<label for="first_name">First Name</label><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" /><br>
-			<label for="last_name">Last Name</label><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" /><br>
-			<label for="email">Email</label><input  id="email" maxlength="80" name="email" size="20" type="text" value="<?php echo $email; ?>" /><br>
+			<label for="first_name">First Name</label><input  id="first_name" maxlength="40" name="first_name" size="20" type="text" required /><br>
+			<label for="last_name">Last Name</label><input  id="last_name" maxlength="80" name="last_name" size="20" type="text" required /><br>
+			<label for="email">Email</label><input  id="email" maxlength="80" name="email" size="20" type="text" value="<?php echo $email; ?>" required /><br>
 			<label for="company">Company</label><input  id="company" maxlength="40" name="company" size="20" type="text" /><br>
 			<label for="city">City</label><input  id="city" maxlength="40" name="city" size="20" type="text" /><br>
 			<label for="state">State/Province</label><input  id="state" maxlength="20" name="state" size="20" type="text" /><br><br>
